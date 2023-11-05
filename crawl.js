7488a0a09f716ae3f75321d1d023fbc7465bdb43
Alvphil <philip.nyblom@gmail.com>
2023-11-05 01:31:47 +0100

    added mote tests and made the normalizer better

--- crawl.js
+++ crawl.js
@@ -1,7 +1,18 @@
+//node:url
+
 function normalizeURL(url){
+    myUrl = new URL(url)
+    url = String(myUrl.hostname+myUrl.pathname)
+    //return(myUrl.hostname+myUrl.pathname)
+    const regex = /(.+?)\/?$/gi;
+    let urlNormal = Array.from(url.matchAll(regex), m => m[1]);
+    return String(urlNormal)
+    
+    /*
     const regex = /https?:\/\/(.+?)\/?$/gi;
     let urlNormal = Array.from(url.matchAll(regex), m => m[1]);
     return String(urlNormal)
+    */
 }
 
 module.exports = {
--- crawl.test.js
+++ crawl.test.js
@@ -16,3 +16,15 @@ test('http://boot.dev/path to boot.dev', () => {
 test('http://boot.dev/path/ to boot.dev', () => {
     expect(normalizeURL('https://boot.dev/path/')).toBe('boot.dev/path')
 });
+
+test('https://boot.dev/path?name=banana to boot.dev', () => {
+    expect(normalizeURL('https://boot.dev/path?name=banana')).toBe('boot.dev/path')
+});
+
+test('https://boot.dev:8000/path?name=banana to boot.dev', () => {
+    expect(normalizeURL('https://boot.dev:8000/path?name=banana')).toBe('boot.dev/path')
+});
+
+test('https://abc:123@boot.dev/path?name=banana to boot.dev', () => {
+    expect(normalizeURL('https://abc:123@boot.dev/path?name=banana')).toBe('boot.dev/path')
+});
