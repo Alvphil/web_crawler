514c6becf48179ac011c051aceb32aa54c171abf
Alvphil <philip.nyblom@gmail.com>
2023-11-08 23:10:06 +0100

    The crawler seems to be working

--- crawl.js
+++ crawl.js
@@ -32,12 +32,55 @@ function getURLsFromHTML(htmlBody, baseURL){
     return urls
 }
 
-function crawlPage(baseURL){
+async function crawlPage(baseURL, currentURL, pages){
+    baUrl = new URL(baseURL);
+    cuUrl = new URL(currentURL);
+    if (baUrl.hostname !== cuUrl.hostname){
+        //console.log(`got to the check ${baUrl.hostname} != ${cuUrl.hostname}`)
+        return pages
+    }
+    normalizedCurrentURL = normalizeURL(currentURL)
+    if (normalizedCurrentURL in pages){
+        pages[normalizedCurrentURL] += 1
+        //console.log(`the url in in pages ${normalizedCurrentURL}`)
+        return pages
+    }
+    else{
+        if (currentURL === baseURL){
+            pages[normalizedCurrentURL] = 0
+        }
+        else{
+            //console.log(`should be adding ${currentURL} to pages, added ${normalizedCurrentURL}`)
+            pages[normalizedCurrentURL] = 1
+        }
+    }
+    let webPage = ""
+    try {
+        webPage = await fetch(currentURL)
+        //console.log(`fetching ${currentURL}`)
+    }catch(err){
+        console.log(err)
+        return
+    }
+    const contentType = await webPage.headers.get("content-type")
+    if (!contentType || !contentType.includes("text/html")){
+        console.log(`Wrong content type.`)
+    }
+    else{
+        const body = await webPage.text()
+        const urlsToCrawl = getURLsFromHTML(body, baseURL)
+        for (url of urlsToCrawl){
+           // console.log(url)
+            pages = await crawlPage(baseURL, url, pages)
+        }
+    }
+    return pages
 
 }
 
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
   }
--- main.js
+++ main.js
@@ -1,18 +1,22 @@
-const { normalizeURL, getURLsFromHTML } = require('./crawl.js')
+const { normalizeURL, getURLsFromHTML, crawlPage } = require('./crawl.js')
 
 
-function main(){
+async function main(){
+    var pages = {}
     if (process.argv.length < 3){
         console.log(`no url provided`)
         process.exit(1);
     }
-    
-    if (process.argv.length === 3) {
-        const url = process.argv[2]
 
-        console.log(`url provided: ${url}`)
-        const normalizedURL = normalizeURL(url)
+    if (process.argv.length === 3) {
+        const baseURL = process.argv[2]
+        const currentURL = baseURL
+        console.log(`url provided: ${baseURL}`)
+        const normalizedURL = normalizeURL(baseURL)
         console.log(`normalized url: ${normalizedURL}`)
+
+        pages = await crawlPage(baseURL, currentURL, pages)
+        console.log(`${JSON.stringify(pages, null, 4)}`)
     }
 }
 
